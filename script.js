cfce27ecdc2cc849b2c97cc46388663a595aee8d
mamandurupraveena <reddypraveen9450@gmail.com>
2026-08-14 15:50:04 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function explore() {
-    document.getElementById("destinations").scrollIntoView({
-        behavior: "smooth"
-    });
-}
-
-function contactMessage() {
-    alert("Thank you for contacting Travel Explorer!");
-}
